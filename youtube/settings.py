8d0bed9fedc1d472fdb6d2fdc1c387d6be858627
claree007 <43228671+claree007@users.noreply.github.com>
2019-09-11 10:23:22 +0530

    Update settings.py

--- youtube/settings.py
+++ youtube/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = '/static/'  #os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static/'  #os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/media/'  #os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'media/'  #os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
