b64aac26dcb5ca3816074575a50802a479acfc2b
Clarence <clarence.t13@gmail.com>
2019-09-12 10:19:06 +0530

    changed something

--- youtube/settings.py
+++ youtube/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#    os.path.join(PROJECT_ROOT, 'transpose/')
+#)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  #'/static/'
 
 STATIC_URL = '/static/'
